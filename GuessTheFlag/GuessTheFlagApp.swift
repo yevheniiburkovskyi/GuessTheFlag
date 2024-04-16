63a447fca2464064b93be7c287cf5159bcedc88d
Yevhenii Burkovskyi <yevhenii.burkovskyi@stormotion.io>
2024-04-16 18:55:01 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Yevhenii Burkovskyi on 16.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
